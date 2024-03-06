945976ff6051d5d7652365303b8c67a1cff00a46
Matt Vella <mcvella@gmail.com>
2024-03-06 18:56:28 -0500

    js api, not yet tested

---
+++ src/speech_service_api_js/grpc/speech_pb.d.ts
@@ -0,0 +1,324 @@
+// package: viamlabs.service.speech.v1
+// file: speech.proto
+
+import * as jspb from "google-protobuf";
+import * as google_api_annotations_pb from "./google/api/annotations_pb";
+
+export class SayRequest extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  getText(): string;
+  setText(value: string): void;
+
+  getBlocking(): boolean;
+  setBlocking(value: boolean): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): SayRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: SayRequest): SayRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: SayRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): SayRequest;
+  static deserializeBinaryFromReader(message: SayRequest, reader: jspb.BinaryReader): SayRequest;
+}
+
+export namespace SayRequest {
+  export type AsObject = {
+    name: string,
+    text: string,
+    blocking: boolean,
+  }
+}
+
+export class SayResponse extends jspb.Message {
+  getText(): string;
+  setText(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): SayResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: SayResponse): SayResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: SayResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): SayResponse;
+  static deserializeBinaryFromReader(message: SayResponse, reader: jspb.BinaryReader): SayResponse;
+}
+
+export namespace SayResponse {
+  export type AsObject = {
+    text: string,
+  }
+}
+
+export class ToTextRequest extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  getSpeech(): Uint8Array | string;
+  getSpeech_asU8(): Uint8Array;
+  getSpeech_asB64(): string;
+  setSpeech(value: Uint8Array | string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ToTextRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: ToTextRequest): ToTextRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ToTextRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ToTextRequest;
+  static deserializeBinaryFromReader(message: ToTextRequest, reader: jspb.BinaryReader): ToTextRequest;
+}
+
+export namespace ToTextRequest {
+  export type AsObject = {
+    name: string,
+    speech: Uint8Array | string,
+  }
+}
+
+export class ToTextResponse extends jspb.Message {
+  getText(): string;
+  setText(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ToTextResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: ToTextResponse): ToTextResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ToTextResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ToTextResponse;
+  static deserializeBinaryFromReader(message: ToTextResponse, reader: jspb.BinaryReader): ToTextResponse;
+}
+
+export namespace ToTextResponse {
+  export type AsObject = {
+    text: string,
+  }
+}
+
+export class ToSpeechRequest extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  getText(): string;
+  setText(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ToSpeechRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: ToSpeechRequest): ToSpeechRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ToSpeechRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ToSpeechRequest;
+  static deserializeBinaryFromReader(message: ToSpeechRequest, reader: jspb.BinaryReader): ToSpeechRequest;
+}
+
+export namespace ToSpeechRequest {
+  export type AsObject = {
+    name: string,
+    text: string,
+  }
+}
+
+export class ToSpeechResponse extends jspb.Message {
+  getSpeech(): Uint8Array | string;
+  getSpeech_asU8(): Uint8Array;
+  getSpeech_asB64(): string;
+  setSpeech(value: Uint8Array | string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ToSpeechResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: ToSpeechResponse): ToSpeechResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ToSpeechResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ToSpeechResponse;
+  static deserializeBinaryFromReader(message: ToSpeechResponse, reader: jspb.BinaryReader): ToSpeechResponse;
+}
+
+export namespace ToSpeechResponse {
+  export type AsObject = {
+    speech: Uint8Array | string,
+  }
+}
+
+export class CompletionRequest extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  getText(): string;
+  setText(value: string): void;
+
+  getBlocking(): boolean;
+  setBlocking(value: boolean): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): CompletionRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: CompletionRequest): CompletionRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: CompletionRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): CompletionRequest;
+  static deserializeBinaryFromReader(message: CompletionRequest, reader: jspb.BinaryReader): CompletionRequest;
+}
+
+export namespace CompletionRequest {
+  export type AsObject = {
+    name: string,
+    text: string,
+    blocking: boolean,
+  }
+}
+
+export class CompletionResponse extends jspb.Message {
+  getText(): string;
+  setText(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): CompletionResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: CompletionResponse): CompletionResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: CompletionResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): CompletionResponse;
+  static deserializeBinaryFromReader(message: CompletionResponse, reader: jspb.BinaryReader): CompletionResponse;
+}
+
+export namespace CompletionResponse {
+  export type AsObject = {
+    text: string,
+  }
+}
+
+export class GetCommandsRequest extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  getNumber(): number;
+  setNumber(value: number): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): GetCommandsRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: GetCommandsRequest): GetCommandsRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: GetCommandsRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): GetCommandsRequest;
+  static deserializeBinaryFromReader(message: GetCommandsRequest, reader: jspb.BinaryReader): GetCommandsRequest;
+}
+
+export namespace GetCommandsRequest {
+  export type AsObject = {
+    name: string,
+    number: number,
+  }
+}
+
+export class GetCommandsResponse extends jspb.Message {
+  clearCommandsList(): void;
+  getCommandsList(): Array<string>;
+  setCommandsList(value: Array<string>): void;
+  addCommands(value: string, index?: number): string;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): GetCommandsResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: GetCommandsResponse): GetCommandsResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: GetCommandsResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): GetCommandsResponse;
+  static deserializeBinaryFromReader(message: GetCommandsResponse, reader: jspb.BinaryReader): GetCommandsResponse;
+}
+
+export namespace GetCommandsResponse {
+  export type AsObject = {
+    commandsList: Array<string>,
+  }
+}
+
+export class ListenTriggerRequest extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  getType(): string;
+  setType(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ListenTriggerRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: ListenTriggerRequest): ListenTriggerRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ListenTriggerRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ListenTriggerRequest;
+  static deserializeBinaryFromReader(message: ListenTriggerRequest, reader: jspb.BinaryReader): ListenTriggerRequest;
+}
+
+export namespace ListenTriggerRequest {
+  export type AsObject = {
+    name: string,
+    type: string,
+  }
+}
+
+export class ListenTriggerResponse extends jspb.Message {
+  getText(): string;
+  setText(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ListenTriggerResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: ListenTriggerResponse): ListenTriggerResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ListenTriggerResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ListenTriggerResponse;
+  static deserializeBinaryFromReader(message: ListenTriggerResponse, reader: jspb.BinaryReader): ListenTriggerResponse;
+}
+
+export namespace ListenTriggerResponse {
+  export type AsObject = {
+    text: string,
+  }
+}
+
+export class IsSpeakingRequest extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): IsSpeakingRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: IsSpeakingRequest): IsSpeakingRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: IsSpeakingRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): IsSpeakingRequest;
+  static deserializeBinaryFromReader(message: IsSpeakingRequest, reader: jspb.BinaryReader): IsSpeakingRequest;
+}
+
+export namespace IsSpeakingRequest {
+  export type AsObject = {
+    name: string,
+  }
+}
+
+export class IsSpeakingResponse extends jspb.Message {
+  getStatus(): boolean;
+  setStatus(value: boolean): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): IsSpeakingResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: IsSpeakingResponse): IsSpeakingResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: IsSpeakingResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): IsSpeakingResponse;
+  static deserializeBinaryFromReader(message: IsSpeakingResponse, reader: jspb.BinaryReader): IsSpeakingResponse;
+}
+
+export namespace IsSpeakingResponse {
+  export type AsObject = {
+    status: boolean,
+  }
+}
+
---
+++ src/speech_service_api_js/interface.ts
@@ -0,0 +1,76 @@
+import * as Viam from '@viamrobotics/sdk';
+
+/** A service that enables speech to text and text to speech */
+export interface Speech extends Viam.Resource {
+  /**
+   * Speak the provided text out loud via speaker/physical audio out device.
+   *
+   * @param text - The name of the camera to use for detection.
+   * @param blocking - If the command should block.
+   * @returns - The spoken text.
+   */
+  say: (
+    text: string,
+    blocking: boolean
+  ) => Promise<string>;
+
+   /**
+   * Convert the provided audio bytes (mp3) to text and return that text.
+   *
+   * @param speech - The bytes of audio in mp3 format.
+   * @returns - The detected text.
+   */
+    toText: (
+        speech: Uint8Array,
+    ) => Promise<string>;
+
+   /**
+   * Convert the provided text to audio bytes and return that audio.
+   *
+   * @param speech - The bytes of audio in mp3 format.
+   * @returns - The converted audio.
+   */
+    toSpeech: (
+        text: string,
+    ) => Promise<Uint8Array>;
+
+   /**
+   * Send the provided text to an LLM completion provider and play the results as speech.
+   *
+   * @param text - The bytes of audio in mp3 format.
+   * @param blocking - If the command should block.
+   * @returns - The text of the completion.
+   */
+    completion: (
+        text: string,
+        blocking: boolean
+    ) => Promise<string>;
+
+   /**
+   * Pull commands from speech buffer and return them as text.
+   *
+   * @param number - The number of commands to return.
+   * @returns - The commands, if any were found.
+   */
+    getCommands: (
+        number: number,
+    ) => Promise<string[]>;
+
+   /**
+   * The next phrase heard will trigger the type of trigger specified.
+   *
+   * @param type - say|completion|command - the type of trigger for the next phrase.
+   * @returns - "OK" if set.
+   */
+    listenTrigger: (
+        type: string,
+    ) => Promise<string>;
+   
+   /**
+   * Is the speech service current speaking out loud?
+   *
+   * @returns - true or false
+   */
+   isSpeaking: (
+    ) => Promise<boolean>;
+}
